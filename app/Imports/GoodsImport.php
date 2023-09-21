0df11de90953bf0f590e3f3ae755f45a53f9c0e1
Romas <infromane@gmail.com>
2023-09-21 13:30:04 +0300

    fix

--- app/Imports/GoodsImport.php
+++ app/Imports/GoodsImport.php
@@ -11,6 +11,7 @@ use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 use Maatwebsite\Excel\Events\AfterImport;
 
 class GoodsImport implements ToModel, WithBatchInserts, WithChunkReading, WithEvents, WithStartRow{
